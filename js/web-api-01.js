255533fc95333a01faa8eac4bed7347bf59bea22
Nathan Long <nlong5@gmail.com>
2022-06-06 11:02:06 -0400

    build 220606

--- js/web-api-01.js
+++ js/web-api-01.js
@@ -1,10 +1,12 @@
 // 01 - Basic Web Animation functions
 
+// setup animation
+
 let tumbling = [
-	{ transform: "rotate(0)", backgroundColor: "green" },
-	{ backgroundColor: "blue", offset: 0.3 },
-	{ backgroundColor: "purple", offset: 0.6 },
-	{ transform: "rotate(360deg)", backgroundColor: "green" },
+	{ transform: "rotate(0)", backgroundColor: "#8ecae6" },
+	{ backgroundColor: "#219ebc", offset: 0.3 },
+	{ backgroundColor: "#023047", offset: 0.6 },
+	{ transform: "rotate(360deg)", backgroundColor: "#8ecae6" },
 ];
 
 let tumblingTiming = {
@@ -20,24 +22,6 @@ const animation = el.animate(tumbling, tumblingTiming);
 
 // so pause it right away
 animation.pause();
-
-// This handler will be executed only once when the cursor
-// moves over the unordered list
-el.addEventListener(
-	"mouseenter",
-	function () {
-		// animation.play();
-	},
-	false
-);
-
-el.addEventListener(
-	"mouseleave",
-	function () {
-		// animation.pause();
-	},
-	false
-);
 
 document.getElementById("spin").addEventListener("mousedown", function () {
 	animation.play();
@@ -56,3 +40,21 @@ document
 document.getElementById("reset").addEventListener("mousedown", function () {
 	animation.playbackRate = animation.playbackRate * 0.8;
 });
+
+const windowWidth = window.innerWidth;
+
+// optionally bind mouse X to playback rate
+document
+	.getElementById("mouse-bind")
+	.addEventListener("mousedown", function () {
+		// raw binding
+		document.onmousemove = handleMouseMove;
+		// measure window on load
+	});
+
+function handleMouseMove(event) {
+	let eventDoc, doc, body;
+	let windowP = event.pageX / windowWidth;
+	// console.log(windowP);
+	animation.playbackRate = windowP * 10;
+}
---
+++ js/web-api-02.js
@@ -0,0 +1,54 @@
+// set up animation
+const appear = [
+	{ opacity: 0, transform: "translateY(10px)" },
+	{ opacity: 1, transform: "translateY(0)" },
+];
+
+const appearOptions = {
+	duration: 800,
+	fill: "forwards", // preserves end state for elements after animation
+	easing: "cubic-bezier(0.33, 1, 0.68, 1)", //easeOutCubic
+};
+
+// ideally we'll be much more specific in our targeted elements
+const observerElements = document.querySelectorAll(".appear-wrapper *");
+
+// begin as soon as the element becomes visible at all in the document
+const observerOptions = { root: null, rootMargin: "0px 0px", threshold: 0 };
+
+// observer function
+const Observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		// set up but pause animation, freezing elements in faded out state
+		let animation = entry.target.animate(appear, appearOptions);
+		animation.pause();
+
+		if (entry.isIntersecting) {
+			// animate the elements unless we're at the top of the window
+			if (window.scrollY > 0) {
+				animation.play();
+			} else {
+				animation.cancel(); // remove animation
+			}
+
+			// remove the observer when done
+			Observer.unobserve(entry.target);
+		}
+	});
+}, observerOptions);
+
+// attach observers
+observerElements.forEach((el) => {
+	Observer.observe(el);
+});
+
+// Features
+//
+// - Does not prevent visibility with JS disabled
+// - Elements should fade in smoothly regardless of scroll speed (no
+//   scroll flash)
+//
+// Wishlist
+//
+// - Stagger elements as they appear? (Avoid batching)
+//
