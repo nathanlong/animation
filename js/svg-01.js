133153d2dc8a9fce3e09a787fb4bb9f59d5be08b
Nathan Long <nlong5@gmail.com>
2022-08-19 17:28:02 -0400

    [svg-01] initial svg exploration

---
+++ js/svg-01.js
@@ -0,0 +1,14 @@
+// svg-01.js
+
+const svg01_01 = document.getElementById('svg-01-mask-animate')
+const svg01root = document.getElementById('svg-01')
+
+document.getElementById('button-svg-01').addEventListener('click', function () {
+	// begins at 0
+	svg01_01.beginElement()
+})
+
+document.getElementById('button-svg-02').addEventListener('click', function () {
+	// ends, seems to interact with fill=freeze
+	svg01_01.endElement()
+})
