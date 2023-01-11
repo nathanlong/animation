5b6a5b1463b81d8953357dcec0a8c6465ab7c755
Nathan Long <nlong5@gmail.com>
2023-01-11 16:31:02 -0500

    [waapi-02] adding staggering based on data values

--- js/waapi-02.js
+++ js/waapi-02.js
@@ -7,43 +7,50 @@
 // When you start with elements at 0 opacity, it will be excluded as an LCP
 // element, but then if it's repainted (like animation) it will result in
 // a high LCP score
+
 const appear = [
-	{ opacity: 0.1, transform: "translateY(10px)" },
-	{ opacity: 1, transform: "translateY(0)" },
-];
+  { opacity: 0, transform: 'translateY(1.5rem)' },
+  // { opacity: 0.1, transform: 'translateY(20px)' },
+  { opacity: 1, transform: 'translateY(0)' },
+]
 
 const appearOptions = {
-	duration: 1200,
-	fill: "forwards", // preserves end state for elements after animation
-	easing: "cubic-bezier(0.33, 1, 0.68, 1)", //easeOutCubic
-};
+  duration: 1250,
+  fill: 'both', // preserves end state for elements after animation
+  easing: 'cubic-bezier(0.33, 1, 0.68, 1)', //easeOutCubic
+}
 
-// ideally we'll be much more specific in our targeted elements
-const observerElements = document.querySelectorAll(".appear-wrapper *");
+// get first descendants of wrapper, and specially targeted elements
+const observerElements = document.querySelectorAll(
+  '.appear-wrapper > *, .appear'
+)
 
 // begin as soon as the element becomes visible at all in the document
-const observerOptions = { root: null, rootMargin: "0px 0px", threshold: 0 };
+const observerOptions = { root: null, rootMargin: '0px 0px', threshold: 0 }
 
 // observer function
 const Observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		// set up but pause animation, freezing elements in faded out state
-		let animation = entry.target.animate(appear, appearOptions);
-		animation.pause();
+  entries.forEach((entry) => {
+    // set up but pause animation, freezing elements in faded out state
+    let animation = entry.target.animate(appear, {
+      ...appearOptions,
+      delay: entry.target.dataset.appearDelay ? parseInt(entry.target.dataset.appearDelay) : 0,
+    })
+    animation.pause()
 
-		if (entry.isIntersecting) {
-			animation.play();
+    if (entry.isIntersecting) {
+      animation.play()
 
-			// remove the observer when done
-			Observer.unobserve(entry.target);
-		}
-	});
-}, observerOptions);
+      // remove the observer when done
+      Observer.unobserve(entry.target)
+    }
+  })
+}, observerOptions)
 
 // attach observers
 observerElements.forEach((el) => {
-	Observer.observe(el);
-});
+  Observer.observe(el)
+})
 
 // Features
 //
@@ -53,5 +60,4 @@ observerElements.forEach((el) => {
 // - No classlist manipulation, all contained in this script
 //
 // Wishlist
-//
-// - Stagger elements as they appear? (Avoid batching)
+// - Decendant animations triggered when parent starts?
