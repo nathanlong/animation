8600ad5600ad47162d9c1b02a3cfe7d36a5afaf9
Nathan Long <nlong5@gmail.com>
2023-01-20 15:35:11 -0500

    [waapi-02] feature: add several data attributes for fine-grain control

--- js/waapi-02.js
+++ js/waapi-02.js
@@ -1,63 +1,110 @@
-// set up animation
+// APPEAR
 //
-// BUT start at non-zero for opacity fade in, this is to help prevent
-// poor LCP (Largest Contentful Paint) scores in automated testing:
-// https://www.debugbear.com/blog/opacity-animation-poor-lcp
+// This module will:
 //
-// When you start with elements at 0 opacity, it will be excluded as an LCP
-// element, but then if it's repainted (like animation) it will result in
-// a high LCP score
-
-const appear = [
-  { opacity: 0, transform: 'translateY(24px)' },
-  // { opacity: 0.1, transform: 'translateY(20px)' },
-  { opacity: 1, transform: 'translateY(0)' },
-]
-
-const appearOptions = {
-  duration: 1250,
-  fill: 'both', // preserves end state for elements after animation
-  easing: 'cubic-bezier(0.33, 1, 0.68, 1)', //easeOutCubic
-}
-
-// get first descendants of wrapper, and specially targeted elements
-const observerElements = document.querySelectorAll('.appear')
-
-// begin as soon as the element becomes visible at all in the document
-const observerOptions = { root: null, rootMargin: '0px 0px', threshold: 0 }
-
-// observer function
-const Observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    // set up but pause animation, freezing elements in faded out state
-    let animation = entry.target.animate(appear, {
-      ...appearOptions,
-      delay: entry.target.dataset.appearDelay
-        ? parseInt(entry.target.dataset.appearDelay)
-        : 0,
-    })
-    animation.pause()
-
-    if (entry.isIntersecting) {
-      animation.play()
-
-      // remove the observer when done
-      Observer.unobserve(entry.target)
-    }
-  })
-}, observerOptions)
-
-// attach observers
-observerElements.forEach((el) => {
-  Observer.observe(el)
-})
-
+// - Search for anything with the class of .appear
+// - Attach an IntersectionObserver to it
+// - When the element enters the viewport, it will animate in (slide up and fade in)
+// - The IntersectionObserver will detach (runs only once per page load)
+//
+// Several options are available to pass as data attributes:
+//
+// - data-appear-delay = time in milliseconds to delay the animation (enables staggered entry)
+// - data-appear-offset = amount in pixels to slide in
+// - data-appear-opacity = value between 0 and 1 to start opacity animation. 
+//   A non-zero value (recommended 0.1) will improve LCP scores for above-the-fold elements.
+// - data-appear-duration = length of animation in milliseconds
+//
 // Features
 //
 // - Does not prevent visibility with JS disabled
-// - Elements should fade in smoothly regardless of scroll speed (no
-//   scroll flash)
-// - No classlist manipulation, all contained in this script
+// - Elements should fade in smoothly regardless of scroll speed (no scroll flash)
 //
 // Wishlist
+//
 // - Decendant animations triggered when parent starts?
+
+
+export default class Appear {
+  constructor(el) {
+    this.el = el
+    this.setVars()
+    this.init()
+  }
+
+  // build animation keyframes, default values if undefined arguments
+  appearAnim(appearOpacity = 0, appearOffset = 24) {
+    return [
+      { opacity: parseInt(appearOpacity), transform: `translateY(${parseInt(appearOffset)}px)`, },
+      { opacity: 1, transform: 'translateY(0)' },
+    ]
+  }
+
+  // build animation options, default values if undefined arguments
+  appearOptions(appearDuration = 1250, appearDelay = 0) {
+    return {
+      duration: parseInt(appearDuration),
+      fill: 'both', // required to persist both start and end states
+      easing: 'cubic-bezier(0.33, 1, 0.68, 1)', //easeOutCubic
+      delay: parseInt(appearDelay)
+    }
+  }
+
+  setVars() {
+    this.observerElements = document.querySelectorAll('.appear')
+    this.observerOptions = { root: null, rootMargin: '0px 0px', threshold: 0 }
+
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        // grab any data attributes
+        const appearOpacity = entry.target.dataset.appearOpacity
+        const appearOffset = entry.target.dataset.appearOffset
+        const appearDuration = entry.target.dataset.appearDuration
+        const appearDelay = entry.target.dataset.appearDelay
+
+        // set up but pause animation, freezing elements in faded out state
+        let animation = entry.target.animate(
+          this.appearAnim(appearOpacity, appearOffset),
+          this.appearOptions(appearDuration, appearDelay)
+        )
+        animation.pause()
+
+        if (entry.isIntersecting) {
+          animation.play()
+
+          // remove the observer when done
+          this.observer.unobserve(entry.target)
+        }
+      })
+    }, this.observerOptions)
+  }
+
+  init() {
+    if (this.observerElements.length > 0) {
+      this.observerElements.forEach((el) => {
+        this.observer.observe(el)
+      })
+    }
+  }
+
+  cleanUp() {
+    if (this.observerElements.length > 0) {
+      this.observerElements.forEach((el) => {
+        this.observer.unobserve(el)
+      })
+    }
+  }
+}
+
+
+// Simplifed version of Viget dynamic modules for demo purposes only
+// DON'T REUSE THIS PATTERN, use the real deal, with HMR and all that...
+// https://www.viget.com/articles/how-does-viget-javascript/
+const dataModules = [...document.querySelectorAll('[data-module="appear"]')]
+
+dataModules.forEach((element) => {
+  element.dataset.module.split(' ').forEach(function () {
+      new Appear(element)
+  })
+})
+
