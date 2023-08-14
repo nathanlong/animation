05183f8344e6430bfb3418ff94c0c3590190723a
Nathan Long <nlong5@gmail.com>
2023-08-14 15:37:44 -0400

    [CSS-02] scroll-based video scrub and others

--- content/css/02-scroll-image-reveal/scripts.js
+++ content/css/02-scroll-image-reveal/scripts.js
@@ -1,30 +1,33 @@
-import { gsap } from '../../public/vendor/gsap-3.10.4/index.js'
+const videoScrubEl = document.querySelector('.video-scrub-cover');
+let vidLength
 
-export default class replaceme {
-  constructor(el) {
-    this.el = el
-    this.setVars()
-    this.bindEvents()
-  }
+videoScrubEl.addEventListener("loadedmetadata", function () {
+	vidLength = Math.floor(videoScrubEl.duration);
+});
 
-  setVars() {
-    this.counter = 0
-  }
+const trackAnimationProgress = (animation, cb, precision = 10) => {
+	let progress = 0;
+	const updateValue = () => {
+		if (animation && animation.currentTime) {
+			let newProgress = animation.effect.getComputedTiming().progress * 1;
+			if (animation.playState === "finished") newProgress = 1;
+			newProgress = Math.max(0.0, Math.min(1.0, newProgress)).toFixed(precision);
 
-  bindEvents() {
-    this.el.addEventListener('click', )
-  }
-
-  handleClick = () => {
-    this.counter = this.counter + 1;
-    console.log(this.counter);
-  }
+			if (newProgress != progress) {
+				progress = newProgress;
+				cb(progress);
+			}
+		}
+		requestAnimationFrame(updateValue);
+	};
+	requestAnimationFrame(updateValue);
 }
 
-const dataModules = [...document.querySelectorAll('[data-module="replaceme"]')]
+document.querySelectorAll('.video-scrub-track').forEach($scrub => {
+	const model = $scrub.querySelector('.video-scrub-cover');
+  console.log($scrub.getAnimations()[0])
 
-dataModules.forEach((element) => {
-  element.dataset.module.split(' ').forEach(function () {
-    new replaceme(element)
-  })
-})
+	trackAnimationProgress($scrub.getAnimations()[0], (progress) => {
+    videoScrubEl.currentTime = progress * vidLength || 0;
+	});
+});
