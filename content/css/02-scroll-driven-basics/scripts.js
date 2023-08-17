6f9fe73a87c9285b0043af81504dd9d6ca41e0f3
Nathan Long <nlong5@gmail.com>
2023-08-17 08:09:10 -0400

    [CSS-02] changing the name, adding some extra stuff

--- content/css/02-scroll-driven-basics/scripts.js
+++ content/css/02-scroll-driven-basics/scripts.js
@@ -1,10 +1,5 @@
-const videoScrubEl = document.querySelector('.video-scrub-cover');
-let vidLength
-
-videoScrubEl.addEventListener("loadedmetadata", function () {
-	vidLength = Math.floor(videoScrubEl.duration);
-});
-
+// Track Animation progress
+// from: https://scroll-driven-animations.style/demos/3d-shoe-explorer/css/
 const trackAnimationProgress = (animation, cb, precision = 10) => {
 	let progress = 0;
 	const updateValue = () => {
@@ -23,9 +18,14 @@ const trackAnimationProgress = (animation, cb, precision = 10) => {
 	requestAnimationFrame(updateValue);
 }
 
+// Set up video scrubbers
 document.querySelectorAll('.video-scrub-track').forEach($scrub => {
-	const model = $scrub.querySelector('.video-scrub-cover');
-  console.log($scrub.getAnimations()[0])
+	const videoScrubEl = $scrub.querySelector('.video-scrub-cover');
+
+  let vidLength
+  videoScrubEl.addEventListener("loadedmetadata", function () {
+    vidLength = Math.floor(videoScrubEl.duration);
+  });
 
 	trackAnimationProgress($scrub.getAnimations()[0], (progress) => {
     videoScrubEl.currentTime = progress * vidLength || 0;
